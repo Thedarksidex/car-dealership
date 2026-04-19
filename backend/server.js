52b1a0e9fd92f5a06f7d1e2bdacef848e4999d6a
Tanmay <sawanttanmay29122004@gmail.com>
2026-04-20 00:55:47 +0530

    fixed cors issue

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,7 @@ const allowedOrigins = [
     'http://localhost:3000',
     'http://127.0.0.1:5500',
     'http://localhost:5500',
+    'http://localhost:5000',
     ...(process.env.ALLOWED_ORIGINS ? process.env.ALLOWED_ORIGINS.split(',').map(o => o.trim()) : []),
 ];
 
